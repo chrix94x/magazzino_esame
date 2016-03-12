e3459702457f62e5a73f667997f101bcba9c53bc
Gian enrico Conti <ingconti@ingconti.com>
2016-03-12 11:13:23 +0100

    added code for TCP in OBJC

--- xcode/TCP_SAMPLE/TCP_SAMPLE/ViewController.m
+++ xcode/TCP_SAMPLE/TCP_SAMPLE/ViewController.m
@@ -8,7 +8,15 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+
+#define HOST @"localhost"
+#define PORT 2000
+
+@interface ViewController () <NSStreamDelegate>
+
+@property NSInputStream *inputStream;
+@property NSOutputStream *outputStream;
+
 
 @end
 
@@ -17,11 +25,88 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+	
+	[self initNetworkCommunication];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+
+- (void)initNetworkCommunication {
+	CFReadStreamRef readStream;
+	CFWriteStreamRef writeStream;
+	CFStreamCreatePairWithSocketToHost(NULL, (CFStringRef)HOST, PORT, &readStream, &writeStream);
+	self.inputStream = (__bridge NSInputStream *)readStream;
+	self.outputStream = (__bridge NSOutputStream *)writeStream;
+	
+	[self.inputStream setDelegate: self];
+	[self.outputStream setDelegate: self];
+	
+	
+	[self.inputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+	[self.outputStream scheduleInRunLoop:[NSRunLoop currentRunLoop] forMode:NSDefaultRunLoopMode];
+	
+	[self.inputStream open];
+	[self.outputStream open];
+	
+	// TODO: close when exiting...
+	
 }
+
+- (IBAction)sendSampleText:(UIButton *)sender {
+	
+	NSString *response  = [NSString stringWithFormat:@"iam:%@", [NSDate date]];
+	NSData *data = [[NSData alloc] initWithData:[response dataUsingEncoding:NSASCIIStringEncoding]];
+	[self.outputStream write:[data bytes] maxLength:[data length]];
+ 
+}
+
+
+
+- (void)stream:(NSStream *)theStream handleEvent:(NSStreamEvent)streamEvent {
+	// NSLog(@"stream event %lu", (unsigned long)streamEvent);
+	
+	
+	switch (streamEvent) {
+			
+		case NSStreamEventOpenCompleted:
+			NSLog(@"Stream opened");
+			break;
+			
+		case NSStreamEventHasBytesAvailable:
+			
+			if (theStream == self.inputStream)
+			{
+				
+				uint8_t buffer[1024];
+				NSInteger len;
+				
+				while ([self.inputStream hasBytesAvailable]) {
+					len = [self.inputStream read:buffer maxLength:sizeof(buffer)];
+					if (len > 0) {
+						
+						NSString *output = [[NSString alloc] initWithBytes:buffer length:len encoding:NSASCIIStringEncoding];
+						
+						if (nil != output) {
+							NSLog(@"server said: %@", output);
+						}
+					}
+				}
+			}
+			
+			break;
+			
+		case NSStreamEventErrorOccurred:
+			NSLog(@"Can not connect to the host!");
+			break;
+			
+		case NSStreamEventEndEncountered:
+			break;
+			
+		default:
+			NSLog(@"Unknown event");
+	}
+}
+
+
 
 @end
