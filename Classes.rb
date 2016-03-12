ea87f4f3845ed91153b6388b20d1331ed2ed389a
christian <chrix94x2@gmail.com>
2016-03-12 12:17:42 +0100

    added error management all queries

--- Classes.rb
+++ Classes.rb
@@ -1,4 +1,9 @@
 
+
+
+
+
+# TODO: try all the errors in queries
 #CREATE CLASS
 class Shop
   def listAll(con,client)
@@ -8,18 +13,8 @@ class Shop
       client.puts results.to_json
       puts results.to_json
     end
-
   rescue Mysql2::Error => e
-    puts e.errno
-    puts e.error.to_json
-
-#err= e.errno  err2=e.error client.puts err.to_json   err2.to_json
-
-    puts "'-------"
-
-
-    h = Hash["errno" => e.errno, "error" => e.error]
-    client.puts h.to_json
+  sendError(client,e)
 
   end
 
@@ -27,34 +22,59 @@ class Shop
    con.query("INSERT INTO magazzino_scorza.prodotti(descrizione,quantita,prezzo,colore,marca,modello,barcode)
       VALUES('#{description}','#{quantity}','#{price}','#{color}','#{brand}','#{model}','#{barcode}')")
     #client.puts "added with no error."
+  rescue Mysql2::Error => e
+    sendError(client,e)
+
   end
 
   def remove(con,client,barcode)
    con.query("UPDATE magazzino_scorza.prodotti SET quantita=quantita-1
    where barcode='#{barcode}'")
     #client.puts "-1 removed"
+  rescue Mysql2::Error => e
+    sendError(client,e)
+
   end
 
   def removeAll(con,client,barcode)
     con.query("UPDATE magazzino_scorza.prodotti SET quantita=0
    where barcode='#{barcode}'")
    # client.puts "removed all"
+  rescue Mysql2::Error => e
+    sendError(client,e)
+
   end
 
   def deleteProduct(con,client,barcode)
     con.query("delete from magazzino_scorza.prodotti
     where barcode='#{barcode}'")
    # client.puts "product deleted"
+  rescue Mysql2::Error => e
+    sendError(client,e)
+
   end
 
   def changePrice(con,client,barcode,price)
     con.query("UPDATE magazzino_scorza.prodotti set prezzo='#{price}' where barcode='#{barcode}'")
   #  client.puts "price changed"
+  rescue Mysql2::Error => e
+    sendError(client,e)
+
   end
 end
 
 
 
+
+
+
+def sendError(client,e)
+  #puts e.errno
+  #puts e.error.to_json
+  h = Hash["errno" => e.errno, "error" => e.error]
+  client.puts h.to_json
+
+end
 
 
 #results= con.query("SELECT * FROM magazzino_scorza.prodotti")
--- example_hash.rb
+++
@@ -1,7 +0,0 @@
-
-
-
-H = Hash["errno" => "10", "msg" => "aaaaaa aaaa"]
-
-puts H
-puts H.to_json
