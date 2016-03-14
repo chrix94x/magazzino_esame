589c20fc15cb2c41007d5487ea3b827b9fe1b87a
christian <chrix94x2@gmail.com>
2016-03-14 13:07:35 +0100

    manage errors or mysql and server not go down

--- magazzino.rb
+++ magazzino.rb
@@ -14,9 +14,10 @@ load 'Classes.rb'
 con = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "yesyesyes")
 
 
-#defines
-localDbServer= 'localhost'
-localport=2000
+#variables for connection
+
+#localDbServer= 'localhost'
+#localport=2000
 
 #server = TCPServer.open(localDbServer,localport)
 
@@ -28,10 +29,11 @@ c= Shop .new
 loop {
 
   #CONNECT TO THE CLIENT
+
   client = server.accept
 
 #client.puts(Time.now.ctime)
- # client.puts(Time.now.ctime+'rubyAllright')                                 # Send the time to the client
+# client.puts(Time.now.ctime+'rubyAllright')                                 # Send the time to the client
 # JSON.parse(string) Method
 
 
@@ -39,15 +41,15 @@ loop {
   readString= client.read
   puts readString
 
-  begin
+begin
     string = JSON.parse(readString)
     analyse(string,c,con,client)
 
-  rescue JSON::ParserError => e
-    client.puts "{\"error\":\"parsejson\"}"
+  #rescue JSON::ParserError => e
+  # client.puts "{\"error\":\"parsejson\"}"
+  # sendError(client,e)
 
-  end
-
+end
 
 
 
@@ -58,39 +60,3 @@ loop {
   client.close
 
 }
-def analyse(string,c,con,client)
-
-  puts "here"
-  puts string
-
-  #ALL VARIABLES
-  command= string["command"]
-  description=string["description"]
-  quantity=string["quantity"]
-  price= string["price"]
-  color= string["color"]
-  brand= string["brand"]
-  model= string["model"]
-  barcode=string["barcode"]
-
-
-  #SWITCH CASE
-  case command
-
-    when "list"
-      c.listAll(con,client)
-    when "insert"
-      c.insert(con,client,description,quantity,price,color,brand,model,barcode)
-    when "remove" #decrementQuantity
-      c.remove(con,client,barcode)
-    when "removeAll" #clearQuantity
-      c.removeAll(con,client,barcode)
-    when "delete"
-      c.deleteProduct(con,client,barcode)
-    when "changePrice"
-      c.changePrice(con,client,barcode,price)
-    else
-      client.puts "commmand not implemented"
-  end
-
-end
