e7d422a1e1100ef96da8d1c63de805ab3437d89b
christian <chrix94x2@gmail.com>
2016-03-18 19:07:11 +0100

    minor fixes

--- xcode/TCP_SAMPLE/TCP_SAMPLE/ViewController.m
+++ xcode/TCP_SAMPLE/TCP_SAMPLE/ViewController.m
@@ -10,8 +10,8 @@
 
 
 
-//#define HOST @"localhost"
-#define HOST @"192.168.2.5"
+#define HOST @"localhost"
+//#define HOST @"192.168.2.5"
 //#define HOST @"104.106.82.112"	// apple
 
 //#define HOST @"192.168.2.5"
